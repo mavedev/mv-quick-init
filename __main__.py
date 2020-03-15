6b952fc5c87427043ed14ccfaddf1f9e76a8d301
Cirill Usatchoff <usatchoff@gmail.com>
2020-03-15 02:40:34 +0200

    CLI fuctionality added

--- __main__.py
+++ __main__.py
@@ -6,7 +6,11 @@ from app.building import AppFactory, AppType
 
 
 def main() -> None:
-    AppFactory.get_app(AppType.PYTHON_APP).create()
+    args: Namespace = get_parsed_arguments()
+    if args.py:
+        AppFactory.get_app(AppType.PYTHON_APP).create()
+    elif args.js:
+        AppFactory.get_app(AppType.JS_APP).create()
 
 
 if __name__ == '__main__':
